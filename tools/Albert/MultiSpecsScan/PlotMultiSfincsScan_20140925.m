c1eb258330f6a714cf12dd4d4bc59306d347cff2
amollen <mollen@nephy.chalmers.se>
2014-10-22 14:04:51 +0200

    Updated plotting script for my tool

--- tools/Albert/MultiSpecsScan/PlotMultiSfincsScan_20140925.m
+++ tools/Albert/MultiSpecsScan/PlotMultiSfincsScan_20140925.m
@@ -9,25 +9,36 @@ excludeRunsThatDidntConverge = true;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Quantities to show
 %QuantitiesToPlot = {'ArrayFirstSpeciesParticleFluxCoefficients'; 'particleFlux'};
-QuantitiesToPlotArray = cellstr({'ArrayFirstSpeciesParticleFluxCoefficients'; 'particleFlux'})
+%QuantitiesToPlotArray = cellstr({'ArrayFirstSpeciesParticleFluxCoefficients'; 'particleFlux'})
+QuantitiesToPlotArray = cellstr({'ArrayFirstSpeciesParticleFluxCoefficients'})
 %QuantitiesOnXaxis = {'nHats'; 'd(PhiHat)d(psi_N)'};
-QuantitiesOnXaxisArray = cellstr({'nHats'; 'd(PhiHat)d(psi_N)'})
-XindicesToPlot = {1 1}; %This is an array of integers which controls which array element to use in the plots for parameters that are vectors in SFINCS.
+%QuantitiesOnXaxisArray = cellstr({'nHats'; 'd(PhiHat)d(psi_N)'})
+QuantitiesOnXaxisArray = cellstr({'nHats'})
+%XindicesToPlot = {1 1};
+XindicesToPlot = {2}; %This is an array of integers which controls which array element to use in the plots for parameters that are vectors in SFINCS.
 %XindicesToPlot must be of the same length as QuantitiesOnXaxisArray and only
 %contain positive integers. '1' should be used if the parameter is a scalar
 %in SFINCS
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Plot options
+set(0, 'defaultTextInterpreter', 'tex'); 
+
+PlotLabel = {'(impurityFlux) = (sfincsFactor) \{ L_{11}^{zz} [(1/n_{z}) (dn_{z}/d\psi) + (Ze/T_{z}) (d\Phi/d\psi) - (3/2T_{z}) (dT_{z}/d\psi)] +', ...
+    '+ L_{11}^{zi} [(1/n_{i}) (dn_{i}/d\psi) + (e/T_{i}) (d\Phi/d\psi) - (3/2T_{i}) (dT_{i}/d\psi)] + L_{12}^{zz} [(1/T_{z})(dT_{z}/d\psi)] + L_{12}^{zi} [(1/T_{i})(dT_{i}/d\psi)]\}'};
+
 FigureWindowSize = 15; 
 figureOffset = 0;
 
 PlotColors = [1,0,0;  0.8,0.6,0;  0,0.7,0;  0,0.8,0.9;  0,0,1;  1,0,1;  0.6,0.6,0.6;  0,0,0];
 
-PlotLinespecs = {'.-r', 'o-g', 'x-b', '.-m', '*-c', '.-r', '.-r', '.-b', '.-m'};
+PlotLinespecs = {'d-r', 'o-g', 'x-b', '.-m', '*-c', '.-r', '.-r', '.-b', '.-m'};
+
+PlotXLogs = {'log', 'log', 'log'}; %'linear' or 'log'
+PlotYLogs = {'log', 'log', 'log'}; %'linear' or 'log'
 
 yAxesLabels = {'L_{11}^{zz}', 'L_{11}^{zi}', 'L_{12}^{zz} + L_{12}^{zi}', '\Gamma_{i}', '\Gamma_{z}'};
-xAxesLabels = {'n_{i}', 'd \Phi / d \psi'};
+xAxesLabels = {'n_{z} (= C \cdot n_{i})', 'd \Phi / d \psi'};
 
 
 leftMargin = 0.1;
@@ -59,20 +70,10 @@ assert(length(QuantitiesToPlotArray) > 0, 'Quantities to plot empty');
 assert(length(QuantitiesOnXaxisArray) > 0, 'Parameter quantities empty');
 
 numRuns = 0;
-%RHSMode1s = 1;
-%RHSMode1s = 0;
-%RHSMode2s = 0;
+
 files = dir();
 dumpedFieldsYet = false;
 didItConverges = [];
-%Nthetas = [];
-%Nzetas = [];
-%Nxis = [];
-%NLs = [];
-%Nxs = [];
-%NxPotentialsPerVths = [];
-%xMaxs = [];
-%log10tols = [];
 outputs = [];
 Nspecies = -1;
 
@@ -131,20 +132,6 @@ for iFile = 1:size(files,1)
         fprintf('Warning: run %d did not converge.\n',i)
     end
 
-    %{
-    RHSMode = h5read(filename,[location,'RHSMode']);
-    switch RHSMode
-        case 1
-            RHSMode1s = RHSMode1s + 1;
-        case 2
-            RHSMode2s = RHSMode2s + 1;            
-        otherwise
-            error('Unrecognized RHSMode')
-    end
-    if RHSMode1s > 0 && RHSMode2s > 0
-        error('Runs must either all have RHSMode=1 or RHSMode=2')
-    end
-     %}
 
     Nspecies_new = h5read(filename,[location,'Nspecies']);
     if Nspecies < 0
@@ -188,72 +175,6 @@ end
 NumberOfPlotCols = ceil(NumberOfFigs / sqrt(NumberOfFigs));
 NumberOfPlotRows = ceil(NumberOfFigs / NumberOfPlotCols);
 
-%return
-%PlotYarray
-%PlotYarray{1,1,1}
-%PlotYarray{1,1,2}
-%PlotYarray{1,1,3}
-%PlotYarray{2,1,1}
-%PlotYarray{2,1,2}
-%PlotYarray{2,1,3}
-%PlotYarray{1,2,1}
-%PlotYarray{1,2,2}
-%PlotYarray{1,2,3}
-%PlotYarray{2,2,1}
-%PlotYarray{2,2,2}
-%PlotYarray{2,2,3}
-%PlotXarray
-%PlotXarray{1,1,1}
-%PlotXarray{1,1,2}
-%PlotXarray{1,1,3}
-%return
-    
-    %Nthetas(runNum) = h5read(filename,[location,'Ntheta']);
-    %Nzetas(runNum) = h5read(filename,[location,'Nzeta']);
-    %Nxis(runNum) = h5read(filename,[location,'Nxi']);
-    %NLs(runNum) = h5read(filename,[location,'NL']);
-    %Nxs(runNum) = h5read(filename,[location,'Nx']);
-    %NxPotentialsPerVths(runNum) = h5read(filename,[location,'NxPotentialsPerVth']);
-    %xMaxs(runNum) = h5read(filename,[location,'xMax']);
-    %log10tols(runNum) = -log10(h5read(filename,[location,'solverTolerance']));
-    %fprintf('%d %d %d %d %d %g %g %g\n',Nthetas(runNum), ...
-    %        Nzetas(runNum),Nxis(runNum),NLs(runNum),Nxs(runNum), ...
-    %        NxPotentialsPerVths(runNum), xMaxs(runNum), log10tols(runNum))
-
-    %outputs(runNum,((1:Nspecies)-1)*3+1) = h5read(filename,[location,'particleFlux']);
-    %outputs(runNum,((1:Nspecies)-1)*3+2) = h5read(filename,[location,'heatFlux']);
-    %outputs(runNum,((1:Nspecies)-1)*3+3) = h5read(filename,[location,'FSABFlow']);
-    %if Nspecies == 1
-    %    outputs(runNum,4) = h5read(filename,[location,'didItConverge']);
-    %    outputs(runNum,5) = h5read(filename,[location,'elapsed time (s)']);
-    %end
-    
-    %{
-    if RHSMode1s > 0
-        outputs(runNum,1) = h5read(filename,[location,'particleFlux']);
-        outputs(runNum,2) = h5read(filename,[location,'heatFlux']);
-        outputs(runNum,3) = h5read(filename,[location,'FSABFlow']);
-        outputs(runNum,4) = h5read(filename,[location,'didItConverge']);
-        outputs(runNum,5) = h5read(filename,[location,'elapsed time (s)']);
-    else
-        transportMatrix = h5read(filename,[location,'transportMatrix']);
-        outputs(runNum,1) = transportMatrix(1,1);
-        outputs(runNum,2) = transportMatrix(1,2);
-        outputs(runNum,3) = transportMatrix(1,3);
-        outputs(runNum,4) = transportMatrix(2,1);
-        outputs(runNum,5) = transportMatrix(2,2);
-        outputs(runNum,6) = transportMatrix(2,3);
-        outputs(runNum,7) = transportMatrix(3,1);
-        outputs(runNum,8) = transportMatrix(3,2);
-        outputs(runNum,9) = transportMatrix(3,3);
-        
-    end
-    %}
-    
-%end
-
-PlotYarray
-PlotXarray
 
 %%PLOT 
 FigPlot = figure(1)
@@ -262,7 +183,6 @@ clf
 set(gcf,'Color','w')
 set(gcf,'Units','inches','Position',[1,1,1.5*FigureWindowSize*NumberOfPlotCols/(NumberOfPlotCols + NumberOfPlotRows),1.0*FigureWindowSize*NumberOfPlotRows/(NumberOfPlotCols + NumberOfPlotRows)])
 
-
 plotHeight = (1-topMargin-bottomMargin-(NumberOfPlotRows - 1)*interPlotVerticalSpacing)/NumberOfPlotRows;
 %plotBottom_topRow = bottomMargin + plotHeight + interPlotVerticalSpacing;
 
@@ -270,20 +190,7 @@ plotWidth = (1-leftMargin-rightMargin-(NumberOfPlotCols - 1)*interPlotHorizontal
 %plotLeft_middleColumn = leftMargin + plotWidth + interPlotHorizontalSpacing;
 %plotLeft_rightColumn = leftMargin + 1*(plotWidth + interPlotHorizontalSpacing);
 
-
-
-%NumberOfPlotRows
-%NumberOfPlotCols
-
 plotNumber = 0;
-
-PlotYarray{1,1,:}
-PlotYarray{1,1,2}(3)
-PlotArrr = PlotYarray{1,1,:}
-PlotArrr(1,:)
-PlotXarray{1,1,:}
-
-%return
 
 Xarray = [];
 Yarray = [];
@@ -293,10 +200,6 @@ for runNum=1:numRuns
     Yarray(runNum) = PlotYarray{1,1,runNum}(2);
 end
 
-Xarray
-Yarray
-
-%return
 
 PosYaxesLabel = 0;
 for j=1:length(QuantitiesToPlotArray)
@@ -309,12 +212,17 @@ for j=1:length(QuantitiesToPlotArray)
                 Yarray(runNum) = PlotYarray{j,k,runNum}(l);
             end
             
+            [XarraySorted, SortIndex] = sort(Xarray);
+            YarraySorted = Yarray(SortIndex);
+            
             plotNumber = plotNumber + 1;
             plotCol = rem(plotNumber - 1,NumberOfPlotCols) + 1;
             plotRow = ceil(plotNumber / NumberOfPlotCols);
             
             PlotColor = [0 0 0];
             PlotLine = 'o-k';
+            PlotXLog = 'linear';
+            PlotYLog = 'linear';
             
             xAxesLabel = '';
             yAxesLabel = '';
@@ -325,6 +233,12 @@ for j=1:length(QuantitiesToPlotArray)
             if plotNumber <= length(PlotLinespecs) 
                 PlotLine = PlotLinespecs{plotNumber};
             end
+            if plotNumber <= length(PlotXLogs) 
+                PlotXLog = PlotXLogs{plotNumber};
+            end
+            if plotNumber <= length(PlotYLogs) 
+                PlotYLog = PlotYLogs{plotNumber};
+            end
             
             if k <= length(xAxesLabels) 
                 xAxesLabel = xAxesLabels{k};
@@ -334,78 +248,22 @@ for j=1:length(QuantitiesToPlotArray)
             end
             
             subplot('Position',[leftMargin + (plotCol - 1)*(plotWidth + interPlotHorizontalSpacing), bottomMargin + (NumberOfPlotRows - plotRow)*(plotHeight + interPlotVerticalSpacing), plotWidth, plotHeight])
-            plot(Xarray, Yarray, PlotLine, 'linewidth', PlotLineWidth, 'MarkerSize', PlotMarkerSize, 'Color', PlotColor)
+            plot(XarraySorted, YarraySorted, PlotLine, 'linewidth', PlotLineWidth, 'MarkerSize', PlotMarkerSize, 'Color', PlotColor)
             
-            set(gca,'XMinorTick','on','YMinorTick','on','XGrid','on','YGrid','on','XMinorGrid','off','YMinorGrid','off', 'FontSize', TickFontSize)
+            set(gca,'XMinorTick','on','YMinorTick','on','XGrid','on','YGrid','on','XMinorGrid','off','YMinorGrid','off', 'FontSize', TickFontSize, 'XScale', PlotXLog, 'YScale', PlotYLog)
             
             xlabel(xAxesLabel,'FontSize',xLabelSize)
             ylabel(yAxesLabel,'FontSize',yLabelSize)
             
-            %length(PlotColors)
-            %length(PlotLinespecs)
-            %PlotColors = [1,0,0;  0.8,0.6,0;  0,0.7,0;  0,0.8,0.9;  0,0,1;  1,0,1;  0.6,0.6,0.6;  0,0,0];
-
-            %PlotLinespecs = {'.-r', '.-g', '.-b', '.-m', '.-c', '.-r', '.-r', '.-b', '.-m'};
-
-            %yAxesLabels = {};
-            %xAxesLabels = {};
         end
     end
     PosYaxesLabel = PosYaxesLabel + length(PlotYarray{j,1,1});
 end
 
+suptitle(PlotLabel);
 
 return;
-
-
-%maxs=ones(numQuantities,1)*(-1e10);
-%mins=ones(numQuantities,1)*(1e10);
-maxs=ones(numQuantities,1)*(-Inf);
-mins=ones(numQuantities,1)*Inf;
-for iParameter = 1:numParameters
-    maxs = max([maxs, quantities{iParameter}'],[],2);
-    mins = min([mins, quantities{iParameter}'],[],2);
-end
-
-
-
-figure(1+figureOffset)
-clf
-set(gcf,'Color','w')
-
-numCols = numParameters;
-
-for iQuantity = 1:numQuantities
-    if maxs(iQuantity) <= mins(iQuantity)
-        maxs(iQuantity) = mins(iQuantity)+1;
-    end
-    for iParameter = 1:numParameters
-        subplot(numRows, numCols, iParameter  + (plotRows(iQuantity) - 1)*numParameters)
-        plot(1./abscissae{iParameter}, quantities{iParameter}(:,iQuantity)', PlotLinespecs{iQuantity})
-        hold on
-        plot(1./[convergeds{iParameter}, convergeds{iParameter}], [mins(iQuantity),maxs(iQuantity)],'k')
-        ylim([mins(iQuantity), maxs(iQuantity)])
-        xlabel(['1/',parametersToVary{iParameter}])
-        ylabel(yAxesLabels{plotRows(iQuantity)})
-    end
-end
-
-temp=dbstack;
-nameOfThisProgram=sprintf('%s',temp(1).file);
-stringForTop = ['Convergence scan from fortran multi-species version of SFINCS, plotted using ',nameOfThisProgram];
-
-
-annotation('textbox',[0 0.96 1 .04],'HorizontalAlignment','center',...
-    'Interpreter','none','VerticalAlignment','bottom',...
-    'FontSize',12,'LineStyle','none','String',stringForTop);
-
-stringForBottom = ['Run in: ',pwd];
-
-annotation('textbox',[0 0 1 .04],'HorizontalAlignment','center',...
-           'Interpreter','none','VerticalAlignment','top',...
-           'FontSize',12,'LineStyle','none','String', ...
-           stringForBottom);
-       
+  
 % --------------------------------------------------------   
 
     function l = getLocationString(runNum)
