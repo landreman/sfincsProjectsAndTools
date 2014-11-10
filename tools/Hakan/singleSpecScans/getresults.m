fd6df55c2fbf2b567f4deb631c2db2dce6753a7b
Håkan Smith <hakan@hejsan.fake>
2014-11-10 17:12:38 +0100

    scan tool updates

--- tools/Hakan/singleSpecScans/getresults.m
+++ tools/Hakan/singleSpecScans/getresults.m
@@ -35,9 +35,16 @@ for hind=1:length(H)
     out.run(ind).dir       =[H{hind}.dirpath,H{hind}.rundir];
     [out.run(ind).time,out.run(ind).proc]=runtime(out.run(ind).dir);
 
+    
     %find out if it is single or multi species version
     multi=isfield(H{hind}.run1,'Nspecies');
       
+    out.geometryScheme(ind) =H{hind}.run1.geometryScheme;
+    if out.geometryScheme(ind)==11
+      out.JGboozer_file{ind} = H{hind}.run1.JGboozer_file;
+    elseif out.geometryScheme(ind)==12
+      out.JGboozer_file_NonStelSym{ind}=H{hind}.run1.JGboozer_file_NonStelSym;
+    end
     if not(multi) %only in single species version
       out.RHSMode(ind)      =H{hind}.run1.RHSMode;
       out.normradius(ind)   =H{hind}.run1.normradius;
