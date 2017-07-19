511541a6425a1f63ec0d925959b2aa3f828472ac
amollen <mollen@nephy.chalmers.se>
2017-07-19 16:49:19 +0200

    Updated plot tools

--- tools/Albert/version3/plot_tools/RadialScanPlotOptions.py
+++ tools/Albert/version3/plot_tools/RadialScanPlotOptions.py
@@ -7,6 +7,7 @@
 ##CAN BE OVERWRITTEN IN ORIGINAL FILE
 
 import matplotlib
+import numpy 
 
 FigSize = (12,10)
 #FigSize = (13.5,10) 
@@ -14,10 +15,14 @@ font = {'size':25}
 matplotlib.rc('font', **font) 
 matplotlib.rc('lines',markeredgewidth=0,markersize=3,linewidth=2.5)
 matplotlib.rc('axes',linewidth=1.5)
+#matplotlib.rc('axes',labelpad=0.0)
+scilimits=(0,0)
+matplotlib.rc('axes.formatter', limits=(0,0))
+#matplotlib.rc('axes.Axes.ticklabel_format',style='sci', axis='y', scilimits=(0,0))
+
 matplotlib.rcParams['mathtext.default'] = 'it'
 matplotlib.rcParams['text.usetex'] = True
 #matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage{amsmath}')
-#matplotlib.rcParams['text.latex.preamble'].append(r'\usepackage{sfmath}')
 
 PlotLinespecs = ['rD-', 'bo-', 'mv-.', 'cs-.', 'yx--', 'g+--', 'kp:', 'k*:'] 
 PlotLineColors = ['r', 'b', 'm', 'c', 'y', 'g', 'k', '0.75'] 
@@ -25,12 +30,16 @@ PlotMarkerEdgeWidth = [3, 3, 3, 0, 3, 3, 3, 3]
 PlotMarkerSize = 10 
 
 xAxisScale = 'linear' #'linear', 'log' or 'symlog'
-yAxisScale = 'linear' #'linear', 'log' or 'symlog' 
+yAxisScale = 'log' #'linear', 'log' or 'symlog' 
 
 ShowGrid = True
 
-xAxisLabel = r'$r/a$' 
-yAxisLabel = r'$<\mathbf{\Gamma}_{e} \cdot \nabla r> $ $[10^{20} \mathrm{m}^{-2} \mathrm{s}^{-1}]$' 
+xAxisLabel = r'$r/a$'
+#yAxisLabel = r'$n_{\mathrm{C}^{6+}}$ $[10^{19} \mathrm{m}^{-3}]$'
+#yAxisLabel = r'$n$ $[10^{19} \mathrm{m}^{-3}]$'
+#yAxisLabel = r'$T$ $[\mathrm{keV}]$'
+yAxisLabel = r'$\nu^{\prime}$'
+#yAxisLabel = r'$<\mathbf{\Gamma}_{\mathrm{C}^{6+}} \cdot \nabla r> $ $[10^{20} \mathrm{m}^{-2} \mathrm{s}^{-1}]$' 
 #yAxisLabel = r'$E_r$ $[\mathrm{kV/m}]$'
 AxesLabelSize = 30
 
@@ -38,8 +47,11 @@ AxisLimAuto = True
 xAxisLim = [0.18, 0.92]
 yAxisLim = [-3.2, 0.0]
 
-PlotLegendLabels = [r'SFINCS Fokker-Planck w/o $\Phi_1$', r'SFINCS Pitch-angle scattering w/o $\Phi_1$', r'SFINCS Fokker-Planck w/ $\Phi_1$', r'SFINCS Pitch-angle scattering w/ $\Phi_1$'] 
+#PlotLegendLabels = [r'SFINCS Fokker-Planck w/o $\Phi_1$', r'SFINCS Pitch-angle scattering w/o $\Phi_1$', r'SFINCS Fokker-Planck w/ $\Phi_1$', r'SFINCS Pitch-angle scattering w/ $\Phi_1$'] 
 #PlotLegendLabels = [r'SFINCS Fokker-Planck w/o $\Phi_1$', r'SFINCS Fokker-Planck w/ $\Phi_1$']
+#PlotLegendLabels = [r'$n_e$', r'$n_i$', r'$n_{\mathrm{He}^{2+}}$', r'$n_{\mathrm{C}^{6+}}$']
+#PlotLegendLabels = [r'$T_e$', r'$T_i$', r'$T_{\mathrm{He}^{2+}}$', r'$T_{\mathrm{C}^{6+}}$']
+PlotLegendLabels = [r'$\nu_{ee}^{\prime}$', r'$\nu_{ii}^{\prime}$', r'$\nu_{HeHe}^{\prime}$', r'$\nu_{CC}^{\prime}$']
 
 #PlotLegendLabels = []
 
@@ -49,6 +61,7 @@ LegendProperties ={'size':'20', 'weight':'heavy'}
 LegendPosition = 3
 LegendNumberColumns = 1
 #LegendBBoxToAnchor = [0.01, 0.01, 1., .102]
+LegendBBoxToAnchor = [0.005, 0.765, 1., .102]
 #LegendBBoxToAnchor = [0.435, 0.765, 1., .102]
-LegendBBoxToAnchor = [0.435, 0.01, 1., .102]
+#LegendBBoxToAnchor = [0.435, 0.01, 1., .102]
 
