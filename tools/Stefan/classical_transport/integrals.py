Fixes SFINCS input reader to convert to psiHat gradients properly from all input. Previously, we had mixed up rN and rHat, oops!
Fixes input reader to interpret negative nu_n values the same way as SFINCS.

tools/Stefan/classical_transport/integrals.py
@@ -2,10 +2,25 @@ from __future__ import division
 
 from scipy.special import erf
 from scipy import exp,sqrt
+from scipy.constants import pi
 from scipy.integrate import quad
 
+def Chandrasehkar(x):
+    return (erf(x) - 2*x*exp(-x**2)/sqrt(pi))/(2*x**2)
 
-b = 5
+def fC1(x,y):
+    return y * x**3 * Chandrasehkar(x) * exp(-y*x**2)
+
+def fC2(x,y):
+    return (y**2 * x**5 - y* x**3) * Chandrasehkar(x) * exp(-y*x**2)
+
+def FC1(y):
+    a,err = quad(fC1,0,float("inf"),args=(y,))
+    return a
+
+def FC2(y):
+    a,err = quad(fC2,0,float("inf"),args=(y,))
+    return a
 
 def f(x,y):
     return erf(x) * x * exp(-y*x**2)
@@ -82,18 +97,19 @@ def Ka(y):
     return (2*y-1)/(8*(1+y)**(5/2))
 
 def F2a(y):
-    return y/(4*(1+y)**(5/2))
+    return 3*y/(8*(1+y)**(5/2))
 
 def Ka2(y):
     #analytical form of H(y) - 3 F(y)/2
-    return 3 *F2a(y)/2 - Fa(y)/2
+    return F2a(y) - Fa(y)/2
 
 if __name__ == "__main__":
     # visualize the above functions
     # and test against known as asymptotes
     
     test1 = False
-    test2 = True
+    test2 = False
+    test3 = True
     import matplotlib.pyplot as plt
     import numpy as np
 
@@ -238,3 +254,33 @@ if __name__ == "__main__":
         axarr[2].legend()
 
         plt.savefig("FHK_analytic.pdf")
+    if test3:
+        y0 = np.linspace(0.5,4)
+
+        # since the functions are not properly
+        # vectorized
+        _FC1 = [FC1(yi) for yi in y0]
+        _F1 = [F(yi) for yi in y0]
+        _FC2 = [FC2(yi) for yi in y0]
+
+        F1 = Fa(y0)
+        F2 = F2a(y0)
+
+        
+        f, axarr = plt.subplots(2, sharex=True)
+        plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        plt.xlabel("y")
+
+        axarr[0].plot(y0,_FC1,'r',label='FC1')
+        axarr[0].plot(y0,F1,'k--',label='F1')
+        axarr[0].plot(y0,_F1,'b:',label='F')
+        axarr[0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        axarr[0].legend()
+
+        axarr[1].plot(y0,_FC2,'r',label='FC2')
+        axarr[1].plot(y0,F2,'k--',label=r'F2')
+        axarr[1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+        axarr[1].legend()
+
+       
+        plt.savefig("F1F2_Chandrasehkar.pdf")

tools/Stefan/classical_transport/read_geometry.py
@@ -26,29 +26,37 @@ def read_geometry(inputRadialCoordinate,rN_wish,Ntheta,Nzeta,equilibriumFile,min
     psiAHat = psiA/(BBar*RBar**2)
 
     a = geom.minorradiusVMEC
+    if a is np.nan:
+        a = geom.minorradiusW7AS
+
+    if a is np.nan:
+        raise ValueError("minor radius from geom object is NaN!")
+    
     aHat = a/RBar
 
     # find which flux surface the use wants to look at
     if inputRadialCoordinate == 0:
         # wish coordinate is psiHat
-        psiN_wish = rN_wish/psiAHat
+        rN_wish = np.sqrt(rN_wish/psiAHat)
     elif inputRadialCoordinate == 1:
         # wish coordinate is psiN
-        psiN_wish = rN_wish
+        rN_wish = np.sqrt(rN_wish)
     elif inputRadialCoordinate == 2:
         # wish coordinate is rHat
-        psiN_wish = rN_wish**2/aHat**2
+        rN_wish = rN_wish/aHat
     elif inputRadialCoordinate == 3:
         # wish coordinate is rN
-        psiN_wish = rN_wish**2
+        rN_wish = rN_wish
+    elif inputRadialCoordinate == 4:
+        # wish coordinate is rHat
+        rN_wish = rN_wish/aHat
+    else:
+        raise ValueError("inputRadialCoordinate should be 0,1,2,3,4; it is" + str(inputRadialCoordinate))
 
-    rind = np.argmin(np.abs(geom.s-psiN_wish))
+
+    rind = np.argmin(np.abs(np.sqrt(geom.s)-rN_wish))
     psiN = geom.s[rind]
-
-
-    #print psiN
-    #print rind
-
+    
     Booz = fluxcoorddiscr(geom,rind,Ntheta,Nzeta,name='Boozer')
     Booz.s = psiN
     Booz.r = np.sqrt(psiN)