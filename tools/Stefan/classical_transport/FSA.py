281c0100e1678f12e03371729b7a569e5144869c
Stefan Buller <sbul@draco01.mpcdf.mpg.de>
2020-03-03 12:40:26 +0100

    Removed JIT from FSA to reduce dependencies.

--- tools/Stefan/classical_transport/FSA.py
+++ tools/Stefan/classical_transport/FSA.py
@@ -1,5 +1,5 @@
 from numpy import sum
-from numba import jit
+#from numba import jit
 
 def FSA_2(X,B):
     # assumes BOOZER coordinates
@@ -10,10 +10,10 @@ def FSA(X,B):
     # sums the two last dimensions
     return sum(X/B**2,axis=(-1,-2))/sum(1/B**2)
 
-@jit
-def jit_FSA(X,B):
-    # assumes BOOZER coordinates
-    return sum(X/B**2)/sum(1/B**2)
+# @jit
+# def jit_FSA(X,B):
+#     # assumes BOOZER coordinates
+#     return sum(X/B**2)/sum(1/B**2)
 
 
 if __name__=="__main__":
@@ -36,10 +36,10 @@ if __name__=="__main__":
             return FSA(X,B)
         return _FSA
 
-    def test_jit_FSA(X,B):
-        def _jit_FSA():
-            return jit_FSA(X,B)
-        return _jit_FSA
+    # def test_jit_FSA(X,B):
+    #     def _jit_FSA():
+    #         return jit_FSA(X,B)
+    #     return _jit_FSA
 
     t=timeit.Timer(test_FSA(A,B))
     print min(t.repeat(100,1))
@@ -47,5 +47,5 @@ if __name__=="__main__":
     t=timeit.Timer(test_FSA(A[0],B))
     print N*min(t.repeat(100,1))
 
-    tjit=timeit.Timer(test_jit_FSA(A[0],B))
-    print N*min(tjit.repeat(100,1))
+    # tjit=timeit.Timer(test_jit_FSA(A[0],B))
+    # print N*min(tjit.repeat(100,1))
